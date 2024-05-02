e7ddd4384b1b8e09e33c1efb8ab7fb6b24829820
Usama726 <usamarashid361@gmail.com>
2024-05-02 18:56:49 +0500

    feat: changed logo

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,8 +2,8 @@
     <div class="w-full py-6 shadow-lg sticky bg-secondary top-[-0.2rem] px-4">
         <div class=" w-full mx-auto max-w-custom z-50 flex items-center justify-between">
             <NuxtLink to="/"
-                class="text-xl relative animate-pulse font-bold italic drop-shadow-xl text-cyan-500 flex  flex-col text-center">
-                <span>TaskMaster</span>
+                class="text-xl relative  font-bold italic drop-shadow-xl text-blue-700 flex gap-1 items-center text-center">
+                <img src="/images/TodoIcon.svg " class="h-7 w-7" /> <span class="animate-pulse">TaskMaster</span>
             </NuxtLink>
             <div>
                 <NuxtLink v-for="(tab, index) in tabs" :key="index" :class="{
